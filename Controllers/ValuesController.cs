abed15e3f40be45c468a0c88105fd716693497ed
Ed. Gi <31946346+dwaxgio@users.noreply.github.com>
2020-08-04 00:02:32 -0500

    Metodo get implementado en controlador

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -11,9 +11,19 @@ namespace APIREST.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<Models.Juegos> Get()
         {
-            return new string[] { "value1", "value2" };
+            // 1. Agregar modelo para mapearlo con entity framework
+            // 2. Agregar controlador (se puede usar el que se construye automaticamente cuando se genera el proyecto
+            // 2.2 Se emplea el contexto ya creado por Entity Framework
+
+            IEnumerable<Models.Juegos> lst;
+            using(Models.ApiRestDBEntities db = new Models.ApiRestDBEntities())
+            {
+                lst = db.Juegos.ToList(); // Para retornar todos los valores de la tabla elejida
+            }
+
+            return lst;
         }
 
         // GET api/values/5
